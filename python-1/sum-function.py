2e39493e4d626066030e2c787e0af0fc54d45819
Bessem <147958693+BessemHuseydi@users.noreply.github.com>
2023-12-09 02:52:43 +0300

    first commit

---
+++ python-1/sum-function.py
@@ -0,0 +1,9 @@
+def sum(x,y):
+    return x+y;
+#kullanicidan sayilari al
+number1=int(input("birinci sayiyi giriniz :"))
+
+number2=int(input("ikinci sayiyi giriniz :"))
+
+sum_numbers=sum(number1,number2)
+print(f"Toplam  :{sum_numbers}")
---
+++ python-2/factorial-calculate.py
@@ -0,0 +1,14 @@
+def factorial_calculate(x):
+    if x < 0:
+        return "Negatif sayilarin faktöriyeli hesaplanamaz."
+    elif x==0 or x==1:
+        return 1
+    else:
+        return x*factorial_calculate(x-1)
+    
+# Kullanıcıdan bir sayı al 
+number=int(input("Faktöriyeli hesaplanacak sayiyi giriniz: "))
+
+factorial=factorial_calculate(number)
+
+print(f"{number}! = {factorial}")
